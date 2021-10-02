5664977f24229d77611a5fa11ab3240acd9a7515
Milen Deyanov <Milen.Deyanov@ibm.com>
2021-10-02 21:04:58 +0300

    database add

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
-const path = require('path');
+// const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
+// const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
-const flash = require('connect-flash');
+// const flash = require('connect-flash');
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const errorController = require('./controllers/error');
+// const User = require('./models/user');
 
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/Assessment'
 
@@ -21,12 +21,12 @@ const app = express()
 
 const csrfProtection = csrf();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user")
+// const adminRoutes = require("./routes/admin");
+// const authRoutes = require("./routes/auth");
+// const userRoutes = require("./routes/user")
 
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'public')));
@@ -41,37 +41,37 @@ const userRoutes = require("./routes/user")
 // app.use(csrfProtection);
 // app.use(flash());
 
-app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   if (!req.session.user) {
+//     return next();
+//   }
+//   User.findById(req.session.user._id)
+//     .then(user => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn
-  res.locals.csrfToken = req.csrfToken()
-  if (req.session.isLoggedIn)
-  res.locals.isRole = req.user.userType
-  next();
-});
+// app.use((req, res, next) => {
+//   res.locals.isAuthenticated = req.session.isLoggedIn
+//   res.locals.csrfToken = req.csrfToken()
+//   if (req.session.isLoggedIn)
+//   res.locals.isRole = req.user.userType
+//   next();
+// });
 
-app.use('/admin', adminRoutes);
-app.use(userRoutes)
-app.use(authRoutes)
+// app.use('/admin', adminRoutes);
+// app.use(userRoutes)
+// app.use(authRoutes)
 
-app.use(errorController.get404);
+// app.use(errorController.get404);
 
 mongoose
   .connect(MONGODB_URI, {
     useNewUrlParser: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
     useUnifiedTopology: true
   })
   .then(result => {
@@ -79,5 +79,5 @@ mongoose
     app.listen(3000);
   })
   .catch(err => {
-    console.log(err + "Connection to database failed!");
+    console.log(err + " Connection to database failed!");
   });
